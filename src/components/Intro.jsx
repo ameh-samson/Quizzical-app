b166fe4b0626a88f1d78cbd5851df656b30cd3de
Samson Ameh <amehsam69@gmail.com>
2023-12-18 15:29:01 +0100

    intro page added

---
+++ src/components/Intro.jsx
@@ -0,0 +1,11 @@
+export default function Intro() {
+  return (
+    <div className="intro-page">
+      <h1 className="intro-header">Quizzical</h1>
+      <h2 className="intro-sub-header">
+        Are you ready to test your intelligence?
+      </h2>
+      <button className="btn">Start quiz</button>
+    </div>
+  );
+}
