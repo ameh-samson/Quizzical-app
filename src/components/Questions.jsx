f33e81f79516232b05c31bd132dba2a19a29120d
Samson Ameh <amehsam69@gmail.com>
2023-12-19 18:38:25 +0100

    hover effect added

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,16 +1,35 @@
+import React, { useState } from "react";
+
 export default function Question(props) {
+  const [selectedOption, setSelectedOption] = useState(null);
+
   const incorrectAnswers = props.questions.incorrect_answers.map(
-    (incorrectAnswer, index) => {
-      return <li key={index}> {incorrectAnswer} </li>;
-    }
+    (incorrectAnswer, index) => (
+      <li key={index} onClick={() => handleOptionClick()}>
+        {incorrectAnswer}
+      </li>
+    )
   );
-  console.log(props.questions);
+
+  const correctAnswer = (
+    <li
+      key={props.questions.correct_answer}
+      onClick={() => handleOptionClick()}
+    >
+      {props.questions.correct_answer}
+    </li>
+  );
+
+  function handleOptionClick() {
+    console.log("option clicked");
+  }
+
   return (
     <div className="questions-section">
       <h2 className="question-title">{props.questions.question}</h2>
       <ul className="question-options">
         {incorrectAnswers}
-        <li>{props.questions.correct_answer}</li>
+        {correctAnswer}
       </ul>
     </div>
   );
